4d78e5816b8c49cf07dc65706c75a8b122234cae
PinghuaZhuang <zphua2016@gmail.com>
2018-05-15 22:05:41 +0800

    Update: eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,26 +27,32 @@ module.exports = {
     ],
     // add your custom rules here
     rules: {
+
         // allow async-await
         'generator-star-spacing': 'off',
+
         // allow debugger during development
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+
         // 缩进
         'indent': [ 'error', 4 ],
+
         // 使用单引号
         'quotes': [ 'error', 'single' ],
+
         // window 下的换行 CRLF
         // 'linebreak-style': [ 'error', 'windows' ],
         // 多个空格
         // a  =    19;
-        'no-multi-spaces': [ 'error', { exceptions: { 'VariableDeclarator': true } } ],
+        // 行尾注释多个空格
+        'no-multi-spaces': [ 'error', { exceptions: { 'VariableDeclarator': true }, ignoreEOLComments: true } ],
+
         // 函数前空格
         'space-before-function-paren': ['error', {
             'anonymous': 'always',      // 匿名函数
             'named': 'always'           // 申明函数
         }],
-        // 行尾注释多个空格
-        'no-multi-spaces': [ 'error', { ignoreEOLComments: true } ],
+
         // 与 null 对比较
         'eqeqeq': [ 'error', 'always', { 'null': 'ignore' } ]
     }
